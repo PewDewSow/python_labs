4ee602702f8011a15209e0d4ee7dba28e501f6c0
Qcx <dkosinov29@gmail.com>
2023-06-21 01:16:49 +0300

    second_task issue

--- lab1/Loops/Loops.py
+++ lab1/Loops/Loops.py
@@ -2,16 +2,31 @@
 # Вам нужно пройти по заполненному списку циклом и записать в пустой список все четные значения
 import random
 
+
 def first_task():
     first_list = []
-    second_list=[]
+    second_list = []
     n = 10
     for i in range(n):
         first_list.append(random.randint(-100, 100))
-    print(f"Первый список = {first_list}")
+    else:
+        print(f"Первый список = {first_list}")
     for i in first_list:
         if i % 2 == 0:
             second_list.append(i)
-    print(f"Второй список = {second_list}")
+    else:
+        print(f"Второй список = {second_list}")
 
-first_task()
+
+# У вас есть строка, нужно пройти по каждому символу и, если он цифра – вывести его
+def second_task():
+    text = "sajdgh312r98tdfbaskj314123sdads"
+    text_final = ''
+    for i in text:
+        if i.isdigit():
+            text_final += i
+    else:
+        print(text_final)
+
+
+second_task()
