892b7fc09b9a571fb5c381be9ef384697355f6b4
Qcx <dkosinov29@gmail.com>
2023-06-21 00:45:05 +0300

    seventh_task.py issue

---
+++ lab1/Сonditions/seventh_task.py
@@ -0,0 +1,5 @@
+# Даны три числа: 130, 25 и 70. Выведите на экран наименьшее из них
+
+first_value, second_value, third_value = 130, 25, 70
+
+print(f"Минимальное число = {min(first_value, second_value, third_value)}")
