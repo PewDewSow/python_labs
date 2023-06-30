49ad334af05456c54b0664abf5050ddcdc042d92
Qcx <dkosinov29@gmail.com>
2023-06-30 22:51:08 +0300

    add lab2 module Sotrudnik.py module OOP_Zadaniya.py

---
+++ lab2/OOP_Zadaniya.py
@@ -0,0 +1,4 @@
+from Sotrudnik import *
+
+vasya = Sotrudnik('Vasya Pupkin', '+7-985-648-4848', 'Manager', 'Sales', 14000)
+misha = Sotrudnik('Misha Popkin', '+7-985-648-4252', 'Black worker', 'IT', 5000)
---
+++ lab2/Sotrudnik.py
@@ -0,0 +1,7 @@
+class Sotrudnik:
+    def __int__(self, fio, tel, post, department, salary):
+        self.fio = fio
+        self.tel = tel
+        self.post = post
+        self.department = department
+        self.salary = salary
